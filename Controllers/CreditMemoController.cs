4fc46193be6ed272b3d9cfd66b7f80120147822e
Sudhakar <sudhakarmallu27@gmail.com>
2025-06-13 17:42:03 +0400

    Committed by sairam for purchase order changes

---
+++ Controllers/CreditMemoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _10xErp.Controllers
+{
+    public class CreditMemoController : Controller
+    {
+        // GET: CreditMemo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
