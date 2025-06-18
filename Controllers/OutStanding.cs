6f1ccaf6e02521d6fe49954d0203219970015cd6
Sudhakar <sudhakarmallu27@gmail.com>
2025-06-18 11:29:30 +0400

    New changes

---
+++ Controllers/OutStanding.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _10xErp.Models;
+using _10xErp.Helpers;
+using System.Data.SqlClient;
+using System.Configuration;
+using _10xErp.ServiceReferenceLayer.SAPB1;
+using Newtonsoft.Json;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace _10xErp.Controllers
+{
+	public class OutStandingController : Controller
+    {
+        private readonly DataHelper objHlpr = new DataHelper(); // Changed to private readonly instance  
+
+        // GET: OutStanding
+        public ActionResult Index()
+        {
+            SalesOrderViewModel oSalesOrderModel = new SalesOrderViewModel();
+
+            //ViewBag.Current = "SalesOrder";
+            return View(oSalesOrderModel);
+        }
+    }
+}
